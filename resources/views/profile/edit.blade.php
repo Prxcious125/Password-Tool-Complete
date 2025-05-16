c1f2fd32d980d3621d9ef466bedace51bb7ebcb5
ajunior9 <your-email@example.com>
2025-05-16 05:27:29 +0100

    Refactor profile settings view to remove redundancy and properly use partial includes

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+@section('title', 'Profile Settings')
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+@section('content')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+        {{-- Just include the partials directly --}}
+        @include('profile.partials.update-profile-information-form')
+
+        @include('profile.partials.update-password-form')
+
+        @include('profile.partials.delete-user-form')
+
     </div>
-</x-app-layout>
+</div>
+
+@if (session('status') === 'profile-updated')
+    <div 
+        x-data="{ show: true }" 
+        x-show="show" 
+        x-transition 
+        x-init="setTimeout(() => show = false, 5000)" 
+        class="fixed bottom-5 right-5 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg"
+    >
+        {{ __('Profile updated successfully!') }}
+    </div>
+@endif
+@endsection
